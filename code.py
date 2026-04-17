66d18b061f4f9bb79125e4585c99fe121c54fdef
skovall <stetskovall@gmail.com>
2026-04-18 00:25:30 +0300

    Update code.py

--- code.py
+++ code.py
@@ -8,7 +8,7 @@ class Bus_Stop:
         self.time_to_next = time_to_next
 
     def show_info(self):
-        return f"Остановка {self.name}\nКоординаты: {self.coordinates}\nВремя до следующей: {self.time}"
+        return f"Остановка {self.name}\nКоординаты: {self.coordinates}\nВремя до следующей: {self.time_to_next}"
 
     def to_dict(self):
             return {
@@ -36,7 +36,7 @@ class Bus_route:
         self.stops = []
 
     def add_stop(self, stop):
-        self.stops += stop
+        self.stops += [stop]
         print(f"В маршрут добавлена остановка {stop.name}.")
 
     def calculate_full_time(self):
@@ -49,17 +49,36 @@ class Bus_route:
     
     def bus_position_after_n_stops(self, current_bus_stop_name, n_stops):
         current_position = self.stops.index(current_bus_stop_name)
-        if current_bus_stop_name + n_stops >= len(self.stops):
+        if current_position + n_stops >= len(self.stops):
             return f"Автобус проедет все {len(self.stops)} остановок и завершит маршрут."
         return f"Через {n_stops} остановок автобус будет на остановке: {self.stops[current_position + n_stops].name}"
     
     def reverse_route(self):
+        if not self.stops:
+            return Bus_route()
+        current_times = []
+        for i in range(len(self.stops) - 2, -1, -1):
+            current_times += [self.stops[i].time_to_next]
+        current_times += [0]
         re_route = Bus_route()
+        n=0
         for stop in reversed(self.stops):
-            reverse_stop = Bus_Stop(stop.name, stop.coordinates, stop.time_to_next)
-            re_route.add_stop(reverse_stop)
+            reverse_stop = Bus_Stop(stop.name, stop.coordinates, current_times[n])
+            re_route.add_stop(reverse_stop)            
+            n+=1
         return re_route
     
-route = Bus_route()
-stop = Bus_Stop('jijo','89.80.90','90')
-route.add_stop(stop)
+    def find_stops_by_time(self, time):
+        result = []
+        full_time = 0
+        for index, stop in enumerate(self.stops):
+            if index > 0:
+                full_time += self.stops[index-1].time_to_next
+            if full_time <= time:
+                result.append({
+                    "name": stop.name,
+                    "position": index,
+                    "full_time": full_time
+                })
+            else: break
+        return result
