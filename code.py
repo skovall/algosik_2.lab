d20f383a1f72ac4908c7f2984bf29f373c204012
skovall <stetskovall@gmail.com>
2026-04-18 01:10:01 +0300

    Update code.py

--- code.py
+++ code.py
@@ -16,20 +16,6 @@ class Bus_Stop:
                 'coordinates': self.coordinates,
                 'time_to_next': self.time_to_next
             }
-    
-    @classmethod
-    def from_dict(cls, data):
-        return cls(data['name'], data['coordinates'], data['time_to_next'])
-    
-    def to_base64(self):
-        json_str = json.dumps(self.to_dict())
-        return base64.b64encode(json_str.encode()).decode()
-    
-    @classmethod
-    def from_base64(cls, base64_str):
-        json_str = base64.b64decode(base64_str).decode()
-        data = json.loads(json_str)
-        return cls.from_dict(data)
 
 class Bus_route:
     def __init__(self):
